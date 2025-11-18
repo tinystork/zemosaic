603fab25ed9adc2e5c41b52dd26f678ea58a3f8e
Tiny Stork <zerealtinystork@gmail.com>
2025-11-18 14:27:26 +0100

    cleanup

--- run_detect.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set PYTHON_EXEC=%~dp0\.venv\Scripts\python.exe
-if exist "%PYTHON_EXEC%" (
-  set PYTHON_CMD="%PYTHON_EXEC%"
-) else (
-  set PYTHON_CMD="python"
-)
-%PYTHON_CMD% detect_pyside.py
-pause
--- test_backend.py
+++
@@ -1,3 +0,0 @@
-import sys
-print(sys.executable)
-print('HAS PySide6?', __import__('importlib.util').find_spec('PySide6') is not None)
